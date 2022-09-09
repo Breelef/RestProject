8f79ce72b74192ab740ae1ea746950d0322d158d
breelef <emil_vinther@hotmail.com>
2022-09-09 10:42:31 +0200

    Metoder lavet

--- src/main/java/com/example/restproject/Service/CourseService.java
+++ src/main/java/com/example/restproject/Service/CourseService.java
@@ -1,34 +1,39 @@
 package com.example.restproject.Service;
 
 import com.example.restproject.Model.Course;
+import com.example.restproject.Repository.CourseRepoInterface;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 @Service
 public class CourseService implements CourseServiceInterface{
+    private CourseRepoInterface repo;
     @Override
     public Set<Course> findAll() {
-        return null;
+        Set <Course> set = new HashSet<>();
+        repo.findAll().forEach(set::add);
+        return set;
     }
 
     @Override
     public Course save(Course object) {
-        return null;
+        return repo.save(object);
     }
 
     @Override
     public void delete(Course object) {
-
+        repo.delete(object);
     }
 
     @Override
     public void deleteById(Long aLong) {
-
+        repo.deleteById(aLong);
     }
 
     @Override
     public Optional<Course> findById(Long aLong) {
-        return Optional.empty();
+        return repo.findById(aLong);
     }
 }
